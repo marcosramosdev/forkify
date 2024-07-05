07c5e72eed2fb9b56fb0976ca689fc0f2fc95d7c
Marcos Ramos <marcosramos.dev@gmail.com>
2024-07-05 09:14:15 -0300

    Adding full application

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '06edbbd0-60b3-4126-a9ff-0dbdbe69b376';
+export const MODAL_CLOSE_SEC = 2.5;
